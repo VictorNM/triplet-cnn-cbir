32d75c42cce5ff446cd962c69a0d3d0e89a621d3
thanh nam <thanhnam.duong.988@gmail.com>
2019-05-07 15:04:11 +0700

    add report

--- src/visualization/visualization.py
+++ src/visualization/visualization.py
@@ -126,16 +126,16 @@ loss_visuale(history.history,loss=False)
 img_visuale(data)
 
 # Visualize CNN Layers
-layer_outputs = [layer.output for layer in model.layers]
-activation_model = Model(inputs=model.input, outputs=layer_outputs)
-activations = activation_model.predict(x_train[10].reshape(1,32,32,3))
+# layer_outputs = [layer.output for layer in model.layers]
+# activation_model = Model(inputs=model.input, outputs=layer_outputs)
+# activations = activation_model.predict(x_train[10].reshape(1,32,32,3))
  
-def display_activation(activations, col_size, row_size, act_index): 
-    activation = activations[act_index]
-    activation_index=0
-    fig, ax = plt.subplots(row_size, col_size, figsize=(row_size*2.5,col_size*1.5))
-    for row in range(0,row_size):
-        for col in range(0,col_size):
-            ax[row][col].imshow(activation[0, :, :, activation_index], cmap='gray')
-            activation_index += 1
+# def display_activation(activations, col_size, row_size, act_index): 
+#     activation = activations[act_index]
+#     activation_index=0
+#     fig, ax = plt.subplots(row_size, col_size, figsize=(row_size*2.5,col_size*1.5))
+#     for row in range(0,row_size):
+#         for col in range(0,col_size):
+#             ax[row][col].imshow(activation[0, :, :, activation_index], cmap='gray')
+#             activation_index += 1
 
