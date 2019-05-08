0c90b6b65cb55e203cadb1ce109995f3b3a0f31e
thanh nam <thanhnam.duong.988@gmail.com>
2019-05-08 09:25:21 +0700

    add file visualization

---
+++ src/visualization/tSNE.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+from keras.datasets import fashion_mnist
+import numpy as np 
+
+import time
+import pandas as pd
+import matplotlib.patheffects as PathEffects 
+
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+def scatter(x,color):
+    pass
