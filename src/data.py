cd96e2acbb21949801255559f9294482a74936e6
victorNM <victor.nguyenmau@gmail.com>
2019-05-18 23:19:17 +0700

    refactor

--- src/data.py
+++ src/data.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-def test():
-    return 'hello'
-
 
 def convert_generator_to_data(generator):
     generator.reset()
@@ -21,3 +18,16 @@ def convert_generator_to_data(generator):
         y[start_index:end_index] = np.argmax(y_batch, axis=1)
 
     return x, y
+
+# def save_pickle(dataset, dataset_name, dataset_type):
+#     pickle_file_name = dataset_name + '.pickle'
+#     pickle_file_path = os.path.join(_data_root, dataset_type, pickle_file_name)
+#     print('Saving dataset to %s' % pickle_file_path)
+#     try:
+#         f = open(pickle_file_path, 'wb')
+#         pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+#         f.close()
+#         print('Saved dataset success')
+#     except Exception as e:
+#         print('Unable to save dataset:', e)
+#         raise
