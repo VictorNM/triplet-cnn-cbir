ab18d624de4eeb94a9dbf0d7786dc3f7444ac07c
victorNM <victor.nguyenmau@gmail.com>
2019-05-26 00:19:25 +0700

    change

--- src/triplet.py
+++ src/triplet.py
@@ -82,6 +82,7 @@ class TripletGenerator(Sequence):
                         min_neg_d = distance
                         min_neg_idx = j
 
+            print(max_pos_d, min_neg_d)
             if max_pos_d + self.margin >= min_neg_d:
                 triplets.append([i, max_pos_idx, min_neg_idx])
 
