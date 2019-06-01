56db2a1b1190a9e39dc09ec0a5d320aec0d9faa8
victorNM <victor.nguyenmau@gmail.com>
2019-06-01 22:19:43 +0700

    change

--- webapp/app.py
+++ webapp/app.py
@@ -4,10 +4,12 @@ sys.path.append(os.path.dirname(sys.path[0]))
 
 from flask import Flask, flash, request, redirect
 from flask import render_template
+from src.models import build_cnn_extractor
 from src.database_large import Database
 from keras.models import load_model
 import shutil
 import ntpath
+from time import time
 
 
 UPLOAD_FOLDER = '../static/upload'
@@ -15,13 +17,15 @@ RESULT_FOLDER = '../static/results'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 TRIPLET_EXTRACTOR_PATH = '../models/final/triplet-2019-05-24 21_06_53.h5'
+CNN_CLASSIFIER_PATH = '../models/final/2019-05-24 21_06_53.h5'
+
 DATABASE_DIRECTORY = '../database/stanford_online_products'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def init_database():
+def init_triplet_database():
     global triplet_db
 
     triplet_extractor = load_model(os.path.join(
@@ -36,7 +40,23 @@ def init_database():
     triplet_db.create_kmeans(2)
 
 
-init_database()
+def init_non_triplet_database():
+    global non_triplet_db
+    cnn_classifier = load_model(os.path.join(
+        os.path.dirname(__file__),
+        CNN_CLASSIFIER_PATH
+    ))
+    cnn_extractor = build_cnn_extractor(cnn_classifier)
+    non_triplet_db = Database(cnn_extractor, os.path.join(
+        os.path.dirname(__file__),
+        DATABASE_DIRECTORY
+    ))
+    non_triplet_db.load_features_database('features-non-triplet')
+    non_triplet_db.create_kmeans(2)
+
+
+init_triplet_database()
+# init_non_triplet_database()
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -60,7 +80,10 @@ def index():
         file.save(abs_img_path)
 
         query_image = triplet_db.load_image(abs_img_path)
-        result_path = triplet_db.query(query_image, use_kmeans=False, num_results=5, return_path=True)
+        start = time()
+        result_path = triplet_db.query(query_image, use_kmeans=False, num_results=12, return_path=True)
+        end = time()
+        query_time = end - start
         client_result_path = []
         for path in result_path:
             image_name = ntpath.basename(path)
@@ -69,4 +92,4 @@ def index():
             shutil.copyfile(path, abs_image_path)
             client_result_path.append(image_path)
 
-        return render_template('index.html', img_path=img_path, result_path=client_result_path)
+        return render_template('index.html', img_path=img_path, result_path=client_result_path, query_time=query_time)
